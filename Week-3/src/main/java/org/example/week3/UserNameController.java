0254046a3ef686b9ac56916b9e1530d7e3e274e9
guo <b22790188@gmail.com>
2024-07-05 13:41:05 +0800

    remove redundent import

--- Week-3/src/main/java/org/example/week3/UserNameController.java
+++ Week-3/src/main/java/org/example/week3/UserNameController.java
@@ -7,7 +7,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class UserNameController {
@@ -47,7 +46,6 @@ public class UserNameController {
 
         return "redirect:/myName";
     }
-
     /**
      * Clear cookie and redirect to myName page
      * @param response used to add cookie.
